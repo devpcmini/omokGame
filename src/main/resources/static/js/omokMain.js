c8c3b1a48c51e80312a8bd592beb175f3c99b707
devpcmini <devpcmini10@gmail.com>
2024-01-10 13:47:28 +0900

    방 입장, 방 나가기, 메세지 , 돌 선택 등 추가

--- src/main/resources/static/js/omokMain.js
+++ src/main/resources/static/js/omokMain.js
@@ -1,72 +1,42 @@
-let roomName;
-let nickName;
 let inBoard = false;
 let myTurn = false;
 let coord = {};
 let isGameEnd = false;
+let publicRoom = [];
+let broadMessage = [];
 
 window.addEventListener("message", function (message) {
-    console.log(message);
     if (message.data instanceof Object) {
         const receivedMessage = message.data;
-        const roomNameEle = document.querySelector('#roomName');
         switch (receivedMessage.type) {
+            case 'room_list' :
+                RoomList({ roomList: receivedMessage.data.map(room => ({ name: room.name }))});
+                break;
             case 'player_select' :
                 myTurn = true;
                 break;
             case 'player_change' :
                 myTurn = false;
-                break;
-            case 'placingStone' :
-                if(!document.querySelector('.turn').classList.contains(receivedMessage.color)){
-                    return;
+                publicRoom[1] = receivedMessage.data.blackPlayer;
+                publicRoom[2] = receivedMessage.data.whitePlayer;
+                if (!(publicRoom[1] == "" || publicRoom[1] == null) &&
+                    !(publicRoom[2] == "" || publicRoom[2] == null)) {
+                    publicRoom[3] = [];
                 }
-                const stone = document.createElement('div');
-                stone.className = 'omok-stone ' + receivedMessage.color + ' ' + seq++;
-                stone.style.top = receivedMessage.row * 40 + 20 + 'px';
-                stone.style.left = receivedMessage.col * 40 + 20 + 'px';
-                stone.setAttribute("data-row", receivedMessage.row);
-                stone.setAttribute("data-col", receivedMessage.col);
-                document.querySelector('.omok-board').appendChild(stone);
-                if(receivedMessage.color == "black"){
-                    document.querySelector('.player.black').classList.remove('turn');
-                    document.querySelector('.player.white').classList.add('turn');
-                } else {
-                    document.querySelector('.player.white').classList.remove('turn');
-                    document.querySelector('.player.black').classList.add('turn')
-                }
-                checkWin();
-                break;
-            case 'winner' :
-                winner = receivedMessage.color;
-                alert(winner + ' wins!');
-                break;
-            case 'create' :
-                roomName =receivedMessage.roomName;
-                roomNameEle.textContent = 'room Name : ' + roomName;
-                nickName = receivedMessage.nickname;
-                if(receivedMessage.stoneColor == "black"){
-                    document.querySelector('.player1').textContent = nickName;
-                } else if(receivedMessage.stoneColor == "white") {
-                    document.querySelector('.player2').textContent = nickName;
-                }
-                userCheck();
-                break;
-            case 'join' :
-                roomName = receivedMessage.roomInfos.roomName;
-                roomNameEle.textContent = 'room Name : ' + roomName;
-                nickName = receivedMessage.nickname;
-                if(document.querySelector('.player1').textContent != 'empty user'){
-                    document.querySelector('.player2').textContent = nickName;
-                } else {
-                    document.querySelector('.player1').textContent = nickName;
-                }
-                break;
-            case 'stoneColorSwitch' :
-
+                GamePanel(publicRoom[0], publicRoom[1], publicRoom[2]);
                 break;
             case 'room_enter' :
-                GamePanel(receivedMessage.data.name, receivedMessage.data.blackPlayer, receivedMessage.data.whitePlayer);
+                broadMessage = [];
+                publicRoom = [receivedMessage.data.name,
+                    receivedMessage.data.blackPlayer,
+                    receivedMessage.data.whitePlayer,
+                    receivedMessage.data.takes
+                ];
+                GamePanel(publicRoom[0], publicRoom[1], publicRoom[2]);
+                break;
+            case 'message' :
+                broadMessage.push(receivedMessage.data);
+                GamePanel(publicRoom[0], publicRoom[1], publicRoom[2]);
                 break;
         }
     }
@@ -76,153 +46,66 @@ function ParentToMessage(message){
     window.parent.postMessage(message);
 }
 
-let seq = 0;
-let winner;
-// dot 클릭 이벤트
-function handleDotClick(e) {
-    if(winner){
+function MessageLine(msg) {
+    return msg + "<br />";
+}
+
+function leaveRoom(){
+    ParentToMessage({type: 'room_leave'});
+}
+
+function moveSpectator() {
+    ParentToMessage({type: 'player_change', data: 'spectator'});
+}
+
+function handleNewRoom(event){
+    event.preventDefault();
+    const name = event.target.roomname.value;
+    event.target.roomname.value = "";
+    if (name.length == 0) {
         return;
     }
-    const row = e.getAttribute("data-row");
-    const col = e.getAttribute("data-col");
-    const message = {type: 'placingStone', color: seq%2==0 ? 'black' : 'white' , row:row , col:col };
-    ParentToMessage(message);
+    const joinRoomMessage = {type: 'room_new', name: name};
+    ParentToMessage(joinRoomMessage);
 }
 
-//가로,세로,대각선, 역대각선으로 다섯 개 이상의 돌이 연속되어 있는지 확인
-function checkWin() {
-    const board = document.querySelector('.omok-board');
-    const stones = Array.from(board.querySelectorAll('.omok-stone'));
-
-    for (const stone of stones) {
-        const stoneColor = stone.classList.contains('black') ? 'black' : 'white';
-        const stonePosition = {
-            top: parseInt(stone.style.top),
-            left: parseInt(stone.style.left)
-        };
-
-        if (checkDirection(board, stoneColor, stonePosition, 1, 0) || // 가로
-            checkDirection(board, stoneColor, stonePosition, 0, 1) || // 세로
-            checkDirection(board, stoneColor, stonePosition, 1, 1) || // 대각선
-            checkDirection(board, stoneColor, stonePosition, 1, -1)) {  // 역대각선
-            const message = {type: 'winner', color: stoneColor };
-            ParentToMessage(message);
-            return;
-        }
-    }
-}
-
-// 특정 위치에서 주어진 방향으로 가로,세로,대각선, 역대각선으로 다섯 개 이상의 돌이 연속되어 있는지 확인
-function checkDirection(board, color, position, directionX, directionY) {
-    const consecutiveStones = [position];
-    for (let i = 1; i < 5; i++) {
-        const nextPosition = {
-            top: position.top + i * directionY * 40,
-            left: position.left + i * directionX * 40
-        };
-        const nextStone = findStoneAtPosition(board, nextPosition);
-        if (nextStone && nextStone.classList.contains(color)) {
-            consecutiveStones.push(nextPosition);
-        } else {
-            break;
-        }
-    }
-    for (let i = 1; i < 5; i++) {
-        const prevPosition = {
-            top: position.top - i * directionY * 40,
-            left: position.left - i * directionX * 40
-        };
-        const prevStone = findStoneAtPosition(board, prevPosition);
-        if (prevStone && prevStone.classList.contains(color)) {
-            consecutiveStones.push(prevPosition);
-        } else {
-            break;
-        }
-    }
-    if (consecutiveStones.length >= 5) {
-        return true;
-    }
-    return false;
-}
-
-// 주어진 위치에 있는 돌을 찾아 반환
-function findStoneAtPosition(board, position) {
-    const stones = Array.from(board.querySelectorAll('.omok-stone'));
-    return stones.find(stone => {
-        const stonePosition = {
-            top: parseInt(stone.style.top),
-            left: parseInt(stone.style.left)
-        };
-        return stonePosition.top === position.top && stonePosition.left === position.left;
+function RoomList(props) {
+    const roomList = props.roomList;
+    const ul = document.querySelector('#room-list-container');
+    ul.innerHTML = '';
+    roomList.forEach(function (roomItem) {
+        const li = RoomItem(roomItem);
+        ul.appendChild(li);
     });
 }
 
-function stoneColorSwitch(e){
-    let target;
-    if(e.id == "player1"){
-        document.querySelector('.player1').textContent = nickName;
-        document.querySelector('.player2').textContent = 'empty user';
-    } else {
-        document.querySelector('.player1').textContent = 'empty user';
-        document.querySelector('.player2').textContent = nickName;
-    }
-    userCheck();
+function RoomItem(room) {
+    const handleEnterRoom = () => {
+        const joinRoomMessage = {type: 'room_enter', name: room.name};
+        ParentToMessage(joinRoomMessage);
+    };
+
+    const li = document.createElement('li');
+    li.className = 'room-list__item';
+
+    const p = document.createElement('p');
+    p.className = 'room-list__name';
+    p.textContent = room.name;
+
+    const button = document.createElement('button');
+    button.className = 'room-list__enter';
+    button.textContent = '입장하기';
+    button.addEventListener('click', handleEnterRoom);
+
+    li.appendChild(p);
+    li.appendChild(button);
+
+    return li;
 }
-
-function userCheck(){
-    if(document.querySelector('.player1').textContent != 'empty user'){
-        document.querySelector('#player1').style.display = 'none';
-    } else {
-        document.querySelector('#player1').style.display = '';
-    }
-    if(document.querySelector('.player2').textContent != 'empty user'){
-        document.querySelector('#player2').style.display = 'none';
-    } else {
-        document.querySelector('#player2').style.display = '';
-    }
-}
-
-function fnGetOut(){
-    const message = {type: 'getOut'};
-    ParentToMessage(message);
-};
-
 
 function GamePanel(roomname, blackPlayer, whitePlayer) {
-    const message = [];
-
-    // socket.on("message", (msg) => {
-    //     message.push(msg);
-    //     render();
-    // });
-
-    function Player({ name, onClick }) {
-        return (
-            "<div class='game-panel__playerinfo'>" +
-            (name !== ""
-                ? "<p class='game-panel__playername'>" + name + "</p>"
-                : "<button class='game-panel__playerselect' onclick='" +
-                onClick +
-                "'>참가</button>") +
-            "</div>"
-        );
-    }
-
-    function MessageLine(msg) {
-        return msg + "<br />";
-    }
-
-    function blackPlayerCallback() {
-        ParentToMessage({type: 'player_change', data : 'black'});
-    }
-
-    function whitePlayerCallback() {
-        ParentToMessage({type: 'player_change', data : 'white'});
-    }
-
-    function render() {
-        const gamePanel = document.querySelector(".game-panel");
-        gamePanel.innerHTML = `
+    const gamePanel = document.querySelector(".game-panel");
+    gamePanel.innerHTML = `
       <div class="game-panel__main">
         <h3 class="game-panel__title">${roomname}</h3>
         <div class="game-panel__players">
@@ -230,25 +113,42 @@ function GamePanel(roomname, blackPlayer, whitePlayer) {
             <h4 class="game-panel__playercolor game-panel__playercolor--black">
               Black
             </h4>
-            ${Player({ name: blackPlayer, onClick: "blackPlayerCallback()" })}
+            <div class="game-panel__playerinfo">
+              ${
+                blackPlayer == "" || blackPlayer == null
+                    ? `<button class="game-panel__playerselect" onclick="blackPlayerCallback()">참가</button>`
+                    : `<p class="game-panel__playername">${blackPlayer}</p>`
+                }
+            </div>
           </div>
           <div class="game-panel__player">
             <h4 class="game-panel__playercolor game-panel__playercolor--white">White</h4>
-            ${Player({ name: whitePlayer, onClick: "whitePlayerCallback()" })}
+            <div class="game-panel__playerinfo">
+              ${
+                  whitePlayer == "" || whitePlayer == null
+                      ? `<button class="game-panel__playerselect" onclick="whitePlayerCallback()">참가</button>`
+                      : `<p class="game-panel__playername">${whitePlayer}</p>`
+              }
+            </div>
           </div>
         </div>
         <div class="game-panel__message">
-          <p>${message.map(MessageLine).join("")}</p>
+          <p>${broadMessage.map(MessageLine).join("")}</p>
         </div>
       </div>
       <div class="game-panel__buttons">
-        <button class="game-panel__button" onclick="ParentToMessage({type: 'player_change', data: 'spectator' })">관전하기</button>
-        <button class="game-panel__button" onclick="ParentToMessage({type: 'room_leave'})">방 나가기</button>
+        <button class="game-panel__button" onclick="moveSpectator()">관전하기</button>
+        <button class="game-panel__button" onclick="leaveRoom()">방 나가기</button>
       </div>
     `;
-    }
+}
 
-    render();
+function blackPlayerCallback() {
+    ParentToMessage({type: 'player_change', data : 'black'});
+}
+
+function whitePlayerCallback() {
+    ParentToMessage({type: 'player_change', data : 'white'});
 }
 
 function handleBoardEnter() {
@@ -260,7 +160,7 @@ function handleBoardLeave() {
 }
 
 function handleBoardMove(thisCoord) {
-    if (takes.find((c) => c.x === thisCoord.x && c.y === thisCoord.y) === undefined) {
+    if (publicRoom[3].find((c) => c.x === thisCoord.x && c.y === thisCoord.y) === undefined) {
         coord = thisCoord;
     }
 }
@@ -271,17 +171,15 @@ function handleBoardSelect() {
     ParentToMessage({type: 'player_selected', data : coord});
 }
 
-function OmokBoard({ takes }) {
-    function render() {
-        const omokBoard = document.querySelector(".omokboard");
-        omokBoard.innerHTML = `
+function OmokBoard(takes) {
+    publicRoom[3] = takes;
+    const omokBoard = document.querySelector(".omokboard");
+    omokBoard.innerHTML = `
       ${myTurn ? CoordSelectArea(handleBoardEnter,handleBoardMove,handleBoardLeave,handleBoardSelect) : ""}
-      ${takes.map((takes, index) => MemoriedStone(index))}
-      ${takes.length > 0 ? MemoriedStone(takes.length - 1, true) : ""}
-      ${myTurn && inBoard ? MemoriedStone(takes.length % 2 === 0 ? "black" : "white", "hint") : ""}
+      ${publicRoom[3].map((takes, index) => MemoriedStone(index))}
+      ${publicRoom[3].length > 0 ? MemoriedStone(publicRoom[3].length - 1, true) : ""}
+      ${myTurn && inBoard ? MemoriedStone(publicRoom[3].length % 2 === 0 ? "black" : "white", "hint") : ""}
     `;
-    }
-    render();
 }
 
 function createStone({ type, x, y }) {
