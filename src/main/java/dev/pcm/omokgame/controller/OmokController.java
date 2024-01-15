1802f2bbe8913124e291765d063a7c8a1b6de33f
devpcmini <devpcmini10@gmail.com>
2024-01-16 00:00:05 +0900

    ip application.properties에서 가져오기

--- src/main/java/dev/pcm/omokgame/controller/OmokController.java
+++ src/main/java/dev/pcm/omokgame/controller/OmokController.java
@@ -1,5 +1,6 @@
 package dev.pcm.omokgame.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +12,23 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/omok")
 public class OmokController {
 
+    @Value("${pcm.server.ip}")
+    private String serverIp;
+
     @GetMapping("/main")
     public String showOmokPage(HttpServletRequest request,Model model) {
-        model.addAttribute("ip",request.getRemoteAddr());
+        model.addAttribute("ip",serverIp);
         return "main";
     }
     @GetMapping("/omokRoomList")
     public String omokRoomList(HttpServletRequest request, Model model) {
-        model.addAttribute("ip",request.getRemoteAddr());
+        model.addAttribute("ip",serverIp);
         return "omokRoomList";
     }
 
     @GetMapping("/omokMain")
     public String showOmokMainPage(HttpServletRequest request, Model model) {
-        model.addAttribute("ip",request.getRemoteAddr());
+        model.addAttribute("ip",serverIp);
         return "omokMain";
     }
 }
