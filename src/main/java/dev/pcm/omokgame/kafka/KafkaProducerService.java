0513d9b8e318288b3779f66596418087a973a43b
devpcmini <devpcmini10@gmail.com>
2024-02-04 19:47:38 +0900

    웹소켓 메시지 수신 시 kafka에 메세지 전달 추가

---
+++ src/main/java/dev/pcm/omokgame/kafka/KafkaProducerService.java
@@ -0,0 +1,18 @@
+package dev.pcm.omokgame.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String TOPIC = "testTopic";
+
+    public void sendKafkaMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+    }
+}
